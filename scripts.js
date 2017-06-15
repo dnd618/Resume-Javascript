4b9020c113ea01907a533b4aa3cfee41c19c27dc
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-15 15:35:59 -0500

    git commit -m "added console logs"

---
+++ scripts.js
@@ -0,0 +1,33 @@
+console.log('dominique deamues'.toUpperCase());
+
+console.log('Software Developer');
+
+console.log('Currently, I am enrolled full-time as a student of the Covalence software bootcamp program. This program is in partnership with "IAmBham" which is an initiative to train & develop underemployed young professionals to become innovative and enter the tech field.')
+
+console.log('My Interests:');
+
+console.log('*Web & Software Design');
+
+console.log('*Shopping');
+
+console.log('*Spending Time with my Family');
+
+console.log('My Previous Experience:');
+
+console.log('* Math, Chemistry, Physics, Biology, and English tutor at Some School')
+
+console.log('- Tutored students in those areas.');
+
+console.log('* Undergraduate Teaching Assistant at University of Georgia');
+
+console.log('Helped grade assignments, answer questions, held lab hours, and held office hours for Java and C++ Computer Science classes.');
+
+console.log('* Lead Programmer for iBeacon Experiment at Georgia Museum of Art'); 
+
+console.log('-Created native iOS virtual tour guide that used iBeacon technology to triangulate position within the museum and give information about apiece.');
+
+
+
+
+
+
